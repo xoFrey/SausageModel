64b98ddfec557ffd83e2bac9d8f527eae69bd953
xofrey <xizelacar@gmail.com>
2025-01-05 14:44:14 +0100

    project

---
+++ src/components/Experience.jsx
@@ -0,0 +1,47 @@
+import { OrbitControls } from "@react-three/drei";
+import { Sausage } from "./Sausage.jsx";
+import { AmbientLight } from "three";
+import { useRef, useEffect } from "react";
+import * as THREE from "three";
+
+const Experience = () => {
+  const spotLightRef = useRef();
+  const helperRef = useRef();
+
+  useEffect(() => {
+    if (spotLightRef.current) {
+      helperRef.current = new THREE.SpotLightHelper(spotLightRef.current);
+      spotLightRef.current.parent.add(helperRef.current);
+    }
+
+    return () => {
+      if (helperRef.current) {
+        helperRef.current.removeFromParent();
+        helperRef.current.dispose();
+      }
+    };
+  }, []);
+
+  return (
+    <>
+      <ambientLight intensity={0.5} />
+      <spotLight
+        intensity={100}
+        distance={100}
+        position={[7, 3, 7]}
+      />
+      <spotLight
+        ref={spotLightRef}
+        intensity={50}
+        distance={10}
+        angle={0.5}
+        position={[-5, 3, -4]}
+      />
+
+      <OrbitControls />
+      <Sausage />
+    </>
+  );
+};
+
+export default Experience;
